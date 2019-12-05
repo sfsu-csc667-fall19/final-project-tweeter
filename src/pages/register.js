eea42c8cb02df894a0c51d7d61aede20a985f2e1
Kevin Venegas <kevinv234@gmail.com>
2019-12-05 01:23:02 -0800

    Added Template and boilerplate code
    Added finishing touches to login and register, create the endpoints for the data that we are sending to mongoDB. Register is going to be touched up, forms need to be alligned

--- src/pages/register.js
+++ src/pages/register.js
@@ -1,19 +1,81 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import axios from 'axios';
 
 
-class Register extends React.component{
-
-    constructor(props){
-        super(props);
-        this.state={
-            first_name:'',
-            last_name:'',
-            email:'',
-            password:''
-        }
+class Register extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      form: {
+        firstname:'',
+        lastname:'',
+        email:'',
+        username: '',
+        password: ''
+      }
     }
+  }
+  
+  submitForm = () => {
+    // console.log(this.state.form.username);
+    // console.log(md5(this.state.form.password));
 
-    
+    const body = {
+      username: this.state.form.username, 
+      password: this.state.form.password
+    };
+
+    axios.post('/register', body)
+      .then((res) => {
+        if (res.data.status === 'success') {
+          
+        } else {
+          console.log("Error registering!");
+        }
+      })
+      .catch(console.log);
+  }
+
+  render() {
+    return (
+      <div>
+        <div className="form-signin">
+          <h1 className="h3 mb-3 font-weight-normal ">Register</h1>
+          <label className="sr-name">First Name</label>
+          <input onChange={e => this.setState({ form: { ...this.state.form, username: e.target.value } })} 
+                  value={this.state.form.firstname} 
+                  type="text" 
+                  className="form-control" 
+                  placeholder="firstname"/>
+          <label>LastName</label>
+          <input onChange={e => this.setState({ form: { ...this.state.form, username: e.target.value } })} 
+                  value={this.state.form.lastname} 
+                  type="text" 
+                  className="form-control" 
+                  placeholder="lastname"/>
+          <label className="sr-only">Username</label>
+          <input onChange={e => this.setState({ form: { ...this.state.form, username: e.target.value } })} 
+                  value={this.state.form.username} 
+                  type="text" 
+                  className="form-control" 
+                  placeholder="Username"/>
+          <label className="sr-only">Password</label>
+          <input onChange={e => this.setState({ form: { ...this.state.form, password: e.target.value } })} 
+                  value={this.state.form.password} 
+                  type="password" 
+                  className="form-control" 
+                  placeholder="Password" />
+          <button onClick={() => this.submitForm()} className="btn btn-lg btn-primary btn-block" type="submit">Submit</button>
+          <button  className="btn btn-lg btn-primary btn-block" type="submit">Cancel</button>
+        </div>
+      </div>
+    );
+  }
 }
 
-export default Register;
+const mapStateToProps = state => ({
+  
+});
+
+export default connect(mapStateToProps)(Register);
--- src/redux/actions/userActions.js
+++ src/redux/actions/userActions.js
@@ -1,8 +1,8 @@
 export const loginUser = (username) => ({
-    type: 'USER_LOGGED_IN',
-    username,
-  })
-  
-  export const logoutUser = () => ({
-    type: 'USER_LOGGED_OUT',
-  })
+  type: 'USER_LOGGED_IN',
+  username,
+})
+
+export const logoutUser = () => ({
+  type: 'USER_LOGGED_IN',
+})
