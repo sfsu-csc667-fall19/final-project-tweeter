f5a17da8e7ad7a0d90573ab699e568fde6c4599a
jason <jasonctle@gmail.com>
2019-12-10 17:23:09 -0800

    Fixed error in user.js and installed 2 dependecy packages

--- server/user.js
+++ server/user.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = require('router');
-const session = require("express-session");
+const session = require("express-sessions");
 
 const { MongoClient, ObjectID } = require('mongodb');
 const app = express();
