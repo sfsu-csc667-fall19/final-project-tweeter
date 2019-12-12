518a72543bd8ed307f520c9ed328d10181ea2732
LiaForel <bkurmant@mail.sfsu.edu>
2019-12-11 20:23:21 -0800

    test

--- server/frontend.js
+++ server/frontend.js
@@ -9,6 +9,6 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '..', 'build', 'index.html'));
 });
 
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || 3000;
 app.listen(port);
 console.log(`Listening on port ${port}`);
